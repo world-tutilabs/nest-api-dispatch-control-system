a8581f70d58f85879c208b3103fc7f98a1bc67ae
paulabssantos <paulablsantos7@gmail.com>
2023-04-12 13:49:15 -0400

    feat: define modulo do banco de dados como global

--- src/main/config/db/database.module.ts
+++ src/main/config/db/database.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { ExpeditionRepositoryInPrisma } from './prisma/repositories/ExpeditionRepositoyInPrisma';
 import { ExpedtitionRepository } from 'src/modules/expedition/repository/ExpeditionRepository';
 
+@Global()
 @Module({
   providers: [{provide: ExpedtitionRepository, useClass: ExpeditionRepositoryInPrisma}],
   exports: [ExpedtitionRepository]
