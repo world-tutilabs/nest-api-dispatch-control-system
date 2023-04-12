78efefbfd5974178b4989acf79e835d8b3bc7496
Luan Albuquerque <luan.santos6605@gmail.com>
2023-04-12 08:43:40 -0400

    config - base de mailer, service

--- src/modules/mail/mail.module.ts
+++ src/modules/mail/mail.module.ts
@@ -1,6 +1,7 @@
 import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
 import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
+import { SendEmailAlertDivergence } from './services/sendEmailAlertDivergence.service';
 @Module({
     imports:[
         MailerModule.forRootAsync({
@@ -28,6 +29,12 @@ import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
               },
             }),
           }),
+    ],
+    providers:[
+        SendEmailAlertDivergence
+    ],
+    exports:[
+        SendEmailAlertDivergence
     ]
 })
 export class MailModule {}
--- src/modules/mail/services/sendEmailAlertDivergence.service.ts
+++ src/modules/mail/services/sendEmailAlertDivergence.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
-export class sendEmailAlertDivergence {
-  constructor(){}
-  async execute() {
+export class SendEmailAlertDivergence {
+  constructor(private readonly mailService: MailerService){}
+  async execute({user}: ISendMailAlertDivergenceDTO) {
+    await this.mailService.sendMail({
+        to: "luan.santos@tutiplast.com.br",
+        from: 'No reply this email please' + process.env.EMAIL_MAIL,
+        subject: 'Alerta de divergencia entre quantidade de atual e quantidade desejada em nota fiscal',
+        template: 'sendEmailAlertDivergence',
+        context: {
+          name: user.name,
+          matricula: user.matricula,
+        },
+      })
+      .catch((error) => {
+        console.log('ERROR SEND EMAIL WITH TOKEN: ' + error);
+      });
+
+
   }
 
 }
