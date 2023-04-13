743136e2781c72e56c8d1e769e1010fe0cefc42a
paulabssantos <paulablsantos7@gmail.com>
2023-04-13 10:23:23 -0400

    feat: implementa handle de erros no guard

--- src/modules/auth/guards/local-auth.guard.ts
+++ src/modules/auth/guards/local-auth.guard.ts
@@ -1,5 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
 
 @Injectable()
-export class LocalAuthGuard extends AuthGuard('local') {}
+export class LocalAuthGuard extends AuthGuard('local') {
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        return super.canActivate(context)
+    }
+
+    handleRequest(err: any, user: any) {
+        if (err || !user) {
+            throw new HttpException(err?.message, HttpStatus.UNAUTHORIZED)
+        }
+        return user
+    }
+}
