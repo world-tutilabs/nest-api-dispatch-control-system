a00a7e4db018bc3e13d3c41e90c8dc8a68112eb1
paulabssantos <paulablsantos7@gmail.com>
2023-04-13 12:46:08 -0400

    docs: cria swagger da rota de login

--- src/modules/auth/useCases/auth.controller.ts
+++ src/modules/auth/useCases/auth.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Request, Post, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from '../guards/local-auth.guard';
 import { AuthService } from './auth.service';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { LoginDTO } from '../dto/login.dto';
 
+@ApiTags("login")
 @Controller('login')
 export class AuthController {
   constructor(private authService: AuthService){}
 
+  @ApiOperation({summary: "Login"})
+  @ApiBody({type: LoginDTO})
   @UseGuards(LocalAuthGuard)
   @Post()
   async login(@Request() req) {
