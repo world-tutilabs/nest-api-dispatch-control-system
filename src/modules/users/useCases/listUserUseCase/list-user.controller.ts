4b4b934af5368e6e00f9bb4d7b9ae6625fe5cc12
Luan Albuquerque <luan.santos6605@gmail.com>
2023-04-13 08:07:21 -0400

    configuração base de swagger

--- src/modules/users/useCases/listUserUseCase/list-user.controller.ts
+++ src/modules/users/useCases/listUserUseCase/list-user.controller.ts
@@ -1,7 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { ListUserService } from './list-user.service';
 import { SendEmailAlertDivergence } from 'src/modules/mail/services/sendEmailAlertDivergence.service';
+import {
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { User } from '../../entities/user.entity';
 
+@ApiTags("Users")
 @Controller('users')
 export class ListUsersController {
   constructor(
@@ -10,6 +16,11 @@ export class ListUsersController {
     ) {}
 
   @Get()
+  @ApiResponse({
+    status: 200,
+    description: 'Listagem de Usuarios',
+    type: User,
+  })
   async handle() {
     await this.email.execute(null)
     return this.listUserService.execute();
