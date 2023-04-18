2c81077fbc22d1d29fbb2747893cd7f0d35632d4
Luan Albuquerque dos Santos <51758832+luan-albuquerque@users.noreply.github.com>
2023-04-18 10:56:30 -0400

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from "@nestjs/mongoose"
 import { IndexModule } from './modules/index.modules';
 import { LogsModule } from './modules/logs/logs.module';
-// import { LogsModule } from './modules/logs/logs.module';
 
 
 @Module({
   imports: [
     IndexModule, 
     ConfigModule.forRoot({ isGlobal: true }), 
-    MongooseModule.forRoot("mongodb://127.0.0.1:27017", {
+    MongooseModule.forRoot("mongodb://mongo_sce", {
       auth:{
         username: "root",
         password: "teste"
