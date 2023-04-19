ea225f5cd7a4eb1a086ecd0b57d7759287142f49
Yan Batista <Yan.Batista@tutiplast.local>
2023-04-19 09:48:18 -0400

    #

--- src/modules/notas-fiscais/notas-fiscais.controller.ts
+++ src/modules/notas-fiscais/notas-fiscais.controller.ts
@@ -1,10 +1,12 @@
 import { NotasFiscaisService } from './notas-fiscais.service';
-import { Controller, Get, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, UseGuards, UseInterceptors } from '@nestjs/common';
 import * as data from '../../../list-nf.json';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CacheInterceptor, CacheKey } from '@nestjs/cache-manager';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('notas-fiscais')
+@UseGuards(JwtAuthGuard)
 @ApiTags('Notas Fiscais')
 export class NotasFiscaisController {
   constructor(private readonly notasFiscaisService: NotasFiscaisService) {}
